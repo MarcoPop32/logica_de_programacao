c4c3b5899bb82dd38a1fbf22ad13c929ea8ded8c
Marcos Paulo <marcosv1881@gmail.com>
2025-06-13 11:43:25 -0400

    switch e case

---
+++ switch_case.js
@@ -0,0 +1,26 @@
+let dias = "sexta";
+
+switch(dias){
+    case "segunda":
+    console.log("Começando a semana");
+    break;
+
+    case "terça":
+    console.log("Mais um dia");
+    break;
+
+    case "quarta":
+    console.log("Estamos na metade da semana uhu!");
+    break;
+
+    case "quinta":
+    console.log("Estamos quase lá");
+    break;
+
+    case "sexta":
+    console.log("Sextou");
+    break;
+
+    default:
+        console.log("Final de semana chegou, uhu!")
+}
